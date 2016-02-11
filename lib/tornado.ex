f8c341d073d591198734efddf0a0074ad745e68a
Dragos Calin <calinho_gx1992@yahoo.com>
2016-02-11 18:00:42 +0200

    Tornado Server Boilerplate

--- lib/tornado.ex
+++ lib/tornado.ex
@@ -1,2 +1,20 @@
 defmodule Tornado do
+	use GenServer
+
+  def start_link(state, opts \\ []) do
+    GenServer.start_link Tornado, state
+  end
+
+  def init(state) do
+    { :ok, state }
+  end
+
+  def handle_call(:get_responses, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_cast({:send_requests, requests}, state) do
+    state = state ++ [{requests, "127.0.0.1\n"}]
+    {:noreply, state }
+  end
 end
--- test/tornado_test.exs
+++ test/tornado_test.exs
@@ -2,7 +2,30 @@ defmodule TornadoTest do
   use ExUnit.Case
   doctest Tornado
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup context do
+    {:ok, request_tuple: {:get, "http://checkip.amazonaws.com/", [], "", []} }
+  end
+
+  test "sending requests with Tornado", %{request_tuple: request_tuple} do
+    { :ok, tornado } = GenServer.start_link Tornado, []
+    assert :ok == GenServer.cast(tornado, {:send_requests, [
+        request_tuple
+      ]})
+  end
+
+  test "getting responses from Tornado", %{request_tuple: request_tuple} do
+    { :ok, tornado } = GenServer.start_link  Tornado, []
+    GenServer.cast(tornado, {:send_requests, [
+      request_tuple
+    ]})
+    responses = GenServer.call(tornado, :get_responses )
+    [{ request_tuple, body }] = responses
+    assert body =~ ~r/(\d){1,3}.(\d){1,3}.(\d){1,3}.(\d){1,3}\n/
+  end
+
+  test "getting responses from Tornado when there are no responses" do
+    { :ok, tornado } = GenServer.start_link  Tornado, []
+    responses = GenServer.call(tornado, :get_responses )
+    assert [] == responses
   end
 end
